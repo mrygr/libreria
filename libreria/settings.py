5413a52cd8658afaf2705cb2444ea46ddbc8e08e
yercin <yer-cin@hotmail.com>
2022-08-17 20:25:51 -0500

    Prueba estilos

--- libreria/settings.py
+++ libreria/settings.py
@@ -16,7 +16,7 @@ import django_heroku
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-_4o_%9i2ghb@4ruy4fk0bg5(3%fxmeze-b9r1d3zpshd1srog6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -128,13 +128,11 @@ USE_TZ = True
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-'''STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )'''
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'Aplicaciones/sistema/static/'), )
 STATIC_URL = '/static/'
 
 django_heroku.settings(locals())
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
