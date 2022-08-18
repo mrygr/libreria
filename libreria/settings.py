9d839cf49af0f89236476f77da54585d013760e4
yercin <yer-cin@hotmail.com>
2022-08-17 22:41:13 -0500

    Prueba estilos

--- libreria/settings.py
+++ libreria/settings.py
@@ -129,9 +129,8 @@ USE_TZ = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-
 
 django_heroku.settings(locals())
 
