6c976491b5ff18ced81926d87d6de7cab70d3de2
yercin <yer-cin@hotmail.com>
2022-08-18 20:19:23 -0500

    Prueba estilos

--- libreria/settings.py
+++ libreria/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'Aplicaciones/sistema/static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 django_heroku.settings(locals())
 
